9138a0ff13a44f57d0d9fda942115bb02c843bc6
Ivan <ivankristiantosantoso5@gmail.com>
2024-07-22 14:20:53 +0700

    done

--- lib/screen/order/order_screen.dart
+++ lib/screen/order/order_screen.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:kitchen/screen/components/order_card.dart';
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   final String selectedCategory;
-  final List<Map<String, dynamic>> dataOrder; // Tambahkan parameter ini
+  final List<Map<String, dynamic>> dataOrder;
 
-  const OrderScreen({Key? key, required this.selectedCategory, required this.dataOrder}) : super(key: key);
+  const OrderScreen({
+    Key? key,
+    required this.selectedCategory,
+    required this.dataOrder,
+  }) : super(key: key);
+
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  double _opacity = 1.0;
+
+  @override
+  void didUpdateWidget(OrderScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.dataOrder != widget.dataOrder) {
+      _fadeOutIn();
+    }
+  }
+
+  void _fadeOutIn() async {
+    setState(() {
+      _opacity = 0.0;
+    });
+    await Future.delayed(const Duration(milliseconds: 300));
+    setState(() {
+      _opacity = 1.0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: OrderCard(selectedCategory: selectedCategory, dataOrder: dataOrder), // Pastikan OrderCard menerima dataOrder
+    return AnimatedOpacity(
+      opacity: _opacity,
+      duration: const Duration(milliseconds: 300),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: OrderCard(
+            selectedCategory: widget.selectedCategory,
+            dataOrder: widget.dataOrder,
+          ),
+        ),
+      ),
     );
   }
 }
